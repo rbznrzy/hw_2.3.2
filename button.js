cb8aaf7597fd70cbbecf3d47b64359ac6b593cb9
3hrsforsleep <rbznrzy@gmail.com>
2024-03-10 22:04:08 +0500

    hw_2.3.2 v2

--- button.js
+++ button.js
@@ -1,21 +1,22 @@
-class SubmitButton{
-    constructor(height, width, type, color){
-        this.props = {
-            height: height,
-            width: width,
-            type: type,
-            color: color
-        };
-    }
-    onClick(){
-        console.log(this.props);
-    }
+class SubmitButton {
+  constructor(height, width, type, color) {
+    this.props = {
+      height: height,
+      width: width,
+      type: type,
+      color: color,
+    };
+  }
+  onClick() {
+    console.log(this.props);
+  }
 }
 
 function testButton() {
-    let newButton = new SubmitButton(123, 22, "button", "green");
-    newButton.onClick()
-
+  let newButton = new SubmitButton(123, 22, "button", "green");
+  newButton.onClick();
+  console.log(newButton instanceof SubmitButton);
 }
 
-testButton()
+testButton();
+
